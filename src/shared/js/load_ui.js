53609fafb453d7b317953d9fd1b375b7d49ec6f9
RyanStudioo <150920961+RyanStudioo@users.noreply.github.com>
2025-09-10 18:47:44 +0800

    main page decor

--- src/shared/js/load_ui.js
+++ src/shared/js/load_ui.js
@@ -1,8 +1,17 @@
-function loadUI(){
+function loadUI(transparent=false) {
     fetch('src/shared/html/nav-bar.html')
     .then(response => response.text())
     .then(data => {
-    var navbar = document.getElementById("nav-bar").innerHTML = data;})
+    var navbar = document.getElementById("nav-bar")
+    navbar.innerHTML = data;
+    if (transparent) {
+        navbar.classList.add("transparent");
+    }
+    })
+    .then(() => {
+        document.body.style.animation = "fadeIn 1s forwards";
+        
+    })
 }
 
 function toggleTheme(){
