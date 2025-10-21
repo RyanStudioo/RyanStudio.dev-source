b92ee179367f066e3d0831890b2de493b53861ff
RyanStudioo <150920961+RyanStudioo@users.noreply.github.com>
2025-10-21 22:28:02 +0800

    line clamp fix

--- src/shared/js/storage.js
+++ src/shared/js/storage.js
@@ -7,5 +7,7 @@ export function getTheme() {
     return localStorage.getItem('darkmode') === 'true' | localStorage.getItem('darkmode') === null;
 }
 
+export function
+
 window.getTheme = getTheme;
 window.setTheme = setTheme;
